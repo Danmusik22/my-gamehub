4706a91a23708ba4459041466f6f6ad9bfe3232c
Danmusik22 <Danmusik22@hotmail.com>
2024-12-15 18:11:17 +0100

    Created NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import logo from "../assets/logo.webp";
+
+
+const NavBar = () => {
+  return (
+    <HStack>
+        <Image boxSize={"60px"} src={logo}/>
+        <Text>Navbar</Text>
+    </HStack>
+  )
+}
+
+export default NavBar
